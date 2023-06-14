ae800002d2b3fb0c0b6a0d46afcf7e2bb4d5f9f3
Robert Saunders <rsaund05@icloud.com>
2023-06-13 22:49:35 -0400

    reddit crawler now functional. Unfortunately, the only way for the spider to function is to disable obeying of robots.txt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # reddit_butler
 Personal project web scraper for reddit.com
+
+### How to use
+From the root directory, run the following command:
+```
+scrapy crawl butler -s CLOSESPIDER_ITEMCOUNT=20 -o ./output/output.jsonl
+```
--- reddit_butler/spiders/butler.py
+++ reddit_butler/spiders/butler.py
@@ -8,7 +8,7 @@ import datetime
 import re
 
 class butler(scrapy.Spider):
-    name = "post"
+    name = "butler"
     start_urls = ['https://old.reddit.com']
 
     def parse(self, response):
@@ -18,7 +18,17 @@ class butler(scrapy.Spider):
             return res
         
         next_selector = response.xpath('//span[@class="next-button"]/a/@href')
-        for p in response.xpath('//div[@id="siteTable"]/div[contains(@class, "thing")]'): #every reddit post on the front page is consitered a 'thing'
-            i = ItemLoader(item=PostItem(), selector=p)
-            i.add_xpath('title', ['.//a[@class="title may-blank"]/text()', './/a[@class="title may-blank outbound"]/text()'], MapCompose(str.strip, str.title))
-            i.add_xpath('user', './/p[@class="tagline"]')
+        for p in response.xpath('//div[@id="siteTable"]/div[contains(@class, "thing")]'):
+            l = ItemLoader(item=PostItem(), selector=p)
+            l.add_xpath('title', ['.//a[@class="title may-blank "]/text()', './/a[@class="title may-blank outbound"]/text()'], MapCompose(str.strip, str.title))
+            l.add_xpath('user','.//p[@class="tagline "]/a[1]/text()')
+            l.add_xpath('upvotes','.//div[@class="score likes"]/@title', MapCompose(int))
+            l.add_xpath('comments','.//a[contains(@data-event-action, "comments")]/text()', MapCompose(lambda x:format_comment(x)))
+            l.add_xpath('subreddit', './/p[@class="tagline "]/a[2]/text()')
+            l.add_xpath('content_link', './/a[contains(@data-event-action, "title")]/@href')
+            l.add_xpath('awards', ['.//a[@class="awarding-link"]/@data-count', './/a[@class="awarding-show-more-link"]/text()'], MapCompose(lambda x: format_comment(x)))
+            l.add_xpath('time', './/time/@datetime')
+            yield l.load_item()
+
+        for url in next_selector.extract():
+            yield Request(url, callback=self.parse)
