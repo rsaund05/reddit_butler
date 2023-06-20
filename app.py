7c3c4f67a6c4e2c7d7953f437502926a3bc12770
Robert Saunders <rsaund05@icloud.com>
2023-06-20 11:55:39 -0400

    refined frontend script

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ __version__="0.1.0"
 
 def main():
     #system call to spider
-    if len(sys.argv) == 1: #check if only 1 argument, meaning hit reddit front page
-        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=100", "-a", "start_urls=https://old.reddit.com", "-O", "./output/output.jsonl"])
+    #build list of subs to gather
+    sublist = []
+    for i in sys.argv:
+        if i != "app.py":
+            sublist.append("https://old.reddit.com/r/" + i)
+    if len(sublist) > 0:
+        subs = ','.join(sublist) #stringified list of URLs sepereated by comma
     else:
-        #build list of subs to gather
-        sublist = []
-        for i in sys.argv:
-            if i != "app.py":
-                sublist.append("https://old.reddit.com/r/" + i)
-        subs = ','.join(sublist) #list of URLs built on inputted subreddits, TODO: Make more secure
-        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=100", "-a", "start_urls=" + subs, "-O", "./output/output.jsonl"])
+        subs = "https://old.reddit.com/" #no inputted subreddits, crawl front page instead
+    subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=100", "-a", "start_urls=" + subs, "-O", "./output/output.jsonl"])
 
 if __name__ == "__main__":
     main()
