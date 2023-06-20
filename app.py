033cbb8e18cef4305458b78832a94a75588c3097
Robert Saunders <rsaund05@icloud.com>
2023-06-20 11:45:49 -0400

    itemcount is behaving strangly around the posts, setting 100 actually yields 100 but setting 1 or 2 just defaults to 25 posts, which is the same number of posts per reddit page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ __version__="0.1.0"
 def main():
     #system call to spider
     if len(sys.argv) == 1: #check if only 1 argument, meaning hit reddit front page
-        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=20","-a", "start_urls=https://old.reddit.com", "-O", "./output/output.jsonl"])
+        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=100", "-a", "start_urls=https://old.reddit.com", "-O", "./output/output.jsonl"])
     else:
         #build list of subs to gather
         sublist = []
@@ -15,7 +15,7 @@ def main():
             if i != "app.py":
                 sublist.append("https://old.reddit.com/r/" + i)
         subs = ','.join(sublist) #list of URLs built on inputted subreddits, TODO: Make more secure
-        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=1", "-a", "start_urls=" + subs, "-O", "./output/output.jsonl"])
+        subprocess.run(["scrapy", "crawl", "butler", "-s", "CLOSESPIDER_ITEMCOUNT=100", "-a", "start_urls=" + subs, "-O", "./output/output.jsonl"])
 
 if __name__ == "__main__":
     main()
